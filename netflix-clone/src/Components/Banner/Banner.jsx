1e97e752ad63f4b176a62e3cbf4770ffe62b8204
Brukt <brktlegesse@gmail.com>
2025-02-21 05:44:38 +0300

    Banner

--- netflix-clone/src/Components/Banner/Banner.jsx
+++ netflix-clone/src/Components/Banner/Banner.jsx
@@ -1,44 +1,58 @@
 import React, { useEffect, useState } from 'react'
 import axios from "../../utils/axios";
 import Requests from '../../utils/Requests';
+import "./banner.css"
+
 
 const Banner = () => {
   const [movie, setMovie] = useState({});
-useEffect(() => {
-(async () => {
-    try {
-        const request = await axios.get(Requests.fetchNetflixOriginals)
-        setMovie(requestAnimationFrame.data.results[
+  useEffect(() => {
+    (async () => {
+      try {
+        const request = await axios.get(Requests.fetchNetflixOriginals);
+        console.log(request);
+        setMovie(
+          request?.data.results[
             Math.floor(Math.random() * request.data.results.length)
-        ]);
-    } catch (error) {
+          ]
+        );
+      } catch (error) {
         console.log("error", error);
-    }
-}) ()
-}, []);
+      }
+    })();
+  }, []);
 
-return (
-    <div className='banner'
-    style={{
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
+  return (
+    <div
+      className="banner"
+      style={{
         backgroundSize: "cover",
-        // backgroundImage: `url('https://image.tmdb.org/t/p/original${movie?.backdrop_path}')`,
+        backgroundImage: `url('https://image.tmdb.org/t/p/original${movie?.backdrop_path}')`,
         backgroundPosition: "center",
-        backgroundRepeat: "no-repeat"
-    }}
+        backgroundRepeat: "no-repeat",
+      }}
     >
       <div className="banner__contents">
-        <h1 className='banner__title'>
-{movie?.title || movie?.name || movie?.original_name}
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
         </h1>
-<div className="banner__buttons">
-<button className="banner__button play">Play</button>
-<button className="banner__button play">My List</button>
-</div>
-<h1 className="banner__description">{truncate(movie?.overview, 150)}</h1>
+        <div className="banner__buttons">
+          <button className="banner__button play">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+      
+        <h1 className="banner__description">
+          {truncate(movie?.overview, 150)}
+        </h1>
       </div>
       <div className="banner__fadeBottom" />
     </div>
-  )
-}
+  );
+};
+
 
 export default Banner
--- netflix-clone/src/utils/Requests.jsx
+++ netflix-clone/src/utils/Requests.jsx
@@ -1,14 +1,16 @@
-const API_KEY = import.meta.env.VITE_API_KEY;
+const API_KEY = import.meta.env.VITE_MOVIE_DB_API_KEY;
 
-const Requests = {
-fetchTrending: '/trending/all/week?api_key=${API_KEY}&language=en_US',
-// fetchPopular: '/movie/popular?api_key=${API_KEY}&language=en_US',
-fetchTopRatedMovies: '/movie/top_rated?api_key=${API_KEY}&language=en_US',
-fetchActionMovies: '/genre/movie/list?api_key=${API_KEY}&with_generes=28',
-fetchHorrorMovies: '/genre/movie/list?api_key=${API_KEY}&with_generes=27',
-fetchRomanceMovies: '/genre/movie/list?api_key=${API_KEY}&with_generes=10749',
-fetchDocumentaries: '/genre/movie/list?api_key=${API_KEY}&with_generes=99',
-fetchTVShow: '/tv/popular?api_key=${API_KEY}&language=en_US',
-}
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+    fetchTvShow: `/tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
+};
 
-export default Requests
+
+export default requests
