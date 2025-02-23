5ea22d15d860f5ac902f4cca5711eb1bf10badc4
Brukt <brktlegesse@gmail.com>
2025-02-23 11:12:30 +0300

    Row and Row List

--- netflix-clone/src/Components/Rows/RowList/RowList.jsx
+++ netflix-clone/src/Components/Rows/RowList/RowList.jsx
@@ -1,39 +1,3 @@
-// import React from 'react'
-// import Row from '../Row/Row'
-// import Requests from '../../../utils/Requests'
-
-// function RowList() {
-//   return (
-//     <>
-//       <Row 
-//       title="NETFLIX ORIGINALS"
-//       fetchUrl={Requests.fetchNetflixOriginals}
-//       isLargeRow={true}
-//       />
-//       <Row title="Trending Now" fetchUrl={Requests.fetchTrending} 
-//       />
-//        <Row title="Netflix Originals" fetchUrl={Requests.fetchNetflixOriginals} 
-//       />
-//  <Row title="Top Rated" fetchUrl={Requests.fetchTopRatedMovies} 
-//       />
-//  <Row title="Action Movies" fetchUrl={Requests.fetchActionMovies} 
-//       />
-// <Row title="Comedy Movies" fetchUrl={Requests.fetchComedyMovies} 
-//       />
-// <Row title="Horror Movies" fetchUrl={Requests.fetchHorrorMovies} 
-//       />
-// <Row title="Romance Movies" fetchUrl={Requests.fetchRomanceMovies} 
-//       />
-// <Row title="Documentaries" fetchUrl={Requests.fetchDocumentaries} 
-//       />
-//       <Row title="TV Shows" fetchUrl={Requests.fetchTvShow} 
-//       />
-//     </>
-//   )
-// }
-
-// export default RowList
-
 import React from "react";
 import Row from "../Row/Row";
 import requests from "../../../utils/Requests";
